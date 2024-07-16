057a9aa3fc869fe855801cdbac131db319fe59e4
Ztrolix <xdpxigaming@gmail.com>
2024-07-16 18:26:46 +1000

    Added Template Mod

---
+++ src/client/java/com/ztrolix/zlibs/ZtrolixLibsClient.java
@@ -0,0 +1,10 @@
+package com.ztrolix.zlibs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZtrolixLibsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/ztrolix/zlibs/ZtrolixLibsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ztrolix.zlibs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZtrolixLibsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
