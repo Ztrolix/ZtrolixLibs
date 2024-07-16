a45676ab871b7c0303620ce9b9b3b5e247a7b09a
Ztrolix <xdpxigaming@gmail.com>
2024-07-16 18:45:58 +1000

    Updated ZtrolixLibs.java

--- src/main/java/com/ztrolix/zlibs/mixin/ExampleMixin.java
+++ src/main/java/com/ztrolix/zlibs/mixin/ExampleMixin.java
@@ -1,6 +1,8 @@
 package com.ztrolix.zlibs.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
+
+
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+
 	}
 }
