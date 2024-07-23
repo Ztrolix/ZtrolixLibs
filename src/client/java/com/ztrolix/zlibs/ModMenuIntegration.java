a8e4ec68127fd7a0372dc223bc736ac50b058dd1
Ztrolix <xdpxigaming@gmail.com>
2024-07-23 20:18:37 +1000

    2.0.0-Alpha.5+Fabric

--- src/client/java/com/ztrolix/zlibs/ModMenuIntegration.java
+++ src/client/java/com/ztrolix/zlibs/ModMenuIntegration.java
@@ -4,11 +4,12 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> ZtrolixLibsClient.getConfigScreen(parent);
+        return ZtrolixLibsClient::getConfigScreen;
     }
 }
