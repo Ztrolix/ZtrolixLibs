b51b53ef89ca26cfb11e06b98ab81790fac3d7e0
Ztrolix <xdpxigaming@gmail.com>
2024-08-19 21:36:28 +1000

    Fixed Imports
    Fixed the Imports by cleaning it up.

--- src/client/java/com/ztrolix/zlibs/api/client/languageUtil.java
+++ src/client/java/com/ztrolix/zlibs/api/client/languageUtil.java
@@ -3,7 +3,6 @@ package com.ztrolix.zlibs.api.client;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.resource.language.LanguageManager;
 
 @Environment(EnvType.CLIENT)
 public class languageUtil {
