41b8c0e2874d534bfc1e602579f210a1e22abaff
Ztrolix <xdpxigaming@gmail.com>
2024-08-23 01:35:15 +1000

    Fixed & Block/Item API
    Fixed and Improved the Block and Item API system.

--- src/main/java/com/ztrolix/zlibs/api/custom.java
+++ src/main/java/com/ztrolix/zlibs/api/custom.java
@@ -3,18 +3,28 @@ package com.ztrolix.zlibs.api;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class custom {
-    public static void Item(String itemId) {
-        Item item = new Item(new Item.Settings());
-        Registry.register(Registries.ITEM, itemId, item);
+    public static void Item(String itemID, String modID) {
+        itemID = itemID.toLowerCase();
+        modID = modID.toLowerCase();
+        Item.Settings settings = new Item.Settings();
+        Identifier identifier = Identifier.of(modID.replace("-", "_"), itemID);
+        Registry.register(Registries.ITEM, identifier, new Item(settings));
     }
 
-    public static void Block(String blockID) {
+    public static void Block(String blockID, String modID) {
+        blockID = blockID.toLowerCase();
+        modID = modID.toLowerCase();
         Block block = new Block(AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).strength(1.5F, 6.0F));
-        Registry.register(Registries.BLOCK, blockID, block);
+        Identifier blockIdentifier = Identifier.of(modID.replace("-", "_"), blockID);
+        Block registeredBlock = Registry.register(Registries.BLOCK, blockIdentifier, block);
+        BlockItem blockItem = new BlockItem(registeredBlock, new Item.Settings());
+        Registry.register(Registries.ITEM, blockIdentifier, blockItem);
     }
 }
