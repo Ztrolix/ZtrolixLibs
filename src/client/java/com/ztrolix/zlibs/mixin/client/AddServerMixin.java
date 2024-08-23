48c8d248d70cf105f5f571babc2c258427bf2032
Ztrolix <xdpxigaming@gmail.com>
2024-08-23 15:49:18 +1000

    Fixes Spaces in IP
    Fixes an issue when adding a server but you added a space in the IP.

---
+++ src/client/java/com/ztrolix/zlibs/mixin/client/AddServerMixin.java
@@ -0,0 +1,14 @@
+package com.ztrolix.zlibs.mixin.client;
+
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(net.minecraft.client.gui.screen.multiplayer.AddServerScreen.class)
+public class AddServerMixin {
+    @Redirect(method = "addAndClose", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;getText()Ljava/lang/String;", ordinal = 1))
+    private String trimGetText(TextFieldWidget instance) {
+        return instance.getText().replace(" ", "");
+    }
+}
